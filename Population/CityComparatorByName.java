aef05bb8b484daa9e5470959a93f8c89f3a6d68c
welainom <154926099+welainom@users.noreply.github.com>
2024-12-10 10:06:58 -0800

    Create CityComparatorByName.java

---
+++ Population/CityComparatorByName.java
@@ -0,0 +1,12 @@
+public class CityComparatorByName implements Comparator<City> {
+        public int compareTo(City a, City b) {
+                if (a.getName().equals(b.getName)) {
+                        return a.getPopulation() - b.getPopulation();
+                }
+                return a.getName().compareTo(b.getName());
+        }
+
+        public int equals(City a, City b) {
+                return a.getName().equals(b.getName());
+        }
+}
