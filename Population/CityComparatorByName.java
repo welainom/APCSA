4dec0490819c44119fd0f3cc475f3ed3ae0c1ad4
welainom <154926099+welainom@users.noreply.github.com>
2024-12-13 14:54:59 -0800

    Update CityComparatorByName.java

--- Population/CityComparatorByName.java
+++ Population/CityComparatorByName.java
@@ -1,21 +1,20 @@
 import java.util.Comparator;
 
 /**
- * CityComparatorByName - Used for comparisons and sorting cities by name
- *
- * @author		William Liu
- * @since		12/8/24
+ *     City Comparator By Name. 
+ *     Used to compare cities by name instead of population.
+ *     @author  William Liu
+ *     @since   12/8/24
  */
-
 public class CityComparatorByName implements Comparator<City> {
         public int compare(City a, City b) {
-                if (a.getName().equals(b.getName())) {
-                        return a.getPopulation() - b.getPopulation();
+                if (!a.getName().equals(b.getName())) {
+                        return a.getName().compareTo(b.getName());
                 }
-                return a.getName().compareTo(b.getName());
+                return a.getPopulation() - b.getPopulation();
         }
 
         public boolean equals(City a, City b) {
-                return a.getName() == b.getName();
+                return a.getName().equals(b.getName());
         }
 }
