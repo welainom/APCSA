f0f36fb8d4ff5375dd7ddcd37a6d6c58bd9084f2
welainom <154926099+welainom@users.noreply.github.com>
2025-01-22 20:40:02 -0800

    Add files via upload

--- AnagramMaker/AnagramMaker.java
+++ AnagramMaker/AnagramMaker.java
@@ -7,122 +7,122 @@
  *	@since		1/19/25
  */
 
- import java.util.ArrayList;
- import java.util.Arrays;
- 
- public class AnagramMaker {
-								 
-	 private final String FILE_NAME = "randomWords.txt";	// file containing all words
-	 
-	 private WordUtilities wu;	// the word utilities for building the word
-								 // database, sorting the database,
-								 // and finding all words that match
-								 // a string of characters
-	 
-	 // variables for constraining the print output of AnagramMaker
-	 private int numWords;		// the number of words in a phrase to print
-	 private int maxPhrases;		// the maximum number of phrases to print
-	 private int numPhrases;		// the number of phrases that have been printed
-	 
-	 private ArrayList<String> anagrams;
- 
-	 /*	Initialize the database inside WordUtilities
-	  *	The database of words does NOT have to be sorted for AnagramMaker to work,
-	  *	but the output will appear in order if you DO sort.
-	  */
-	 public AnagramMaker() {
-		 wu = new WordUtilities();
-		 wu.readWordsFromFile(FILE_NAME);
-		 wu.sortWords();
- 
-		 anagrams = new ArrayList<String>();
-		 numPhrases = 0;
-	 }
-	 
-	 public static void main(String[] args) {
-		 AnagramMaker am = new AnagramMaker();
-		 am.run();
-	 }
-	 
-	 /**	The top routine that prints the introduction and runs the anagram-maker.
-	  */
-	 public void run() {
-		 printIntroduction();
-		 runAnagramMaker();
-		 System.out.println("\nThanks for using AnagramMaker!\n");
-	 }
-	 
-	 /**
-	  *	Print the introduction to AnagramMaker
-	  */
-	 public void printIntroduction() {
-		 System.out.println("\nWelcome to ANAGRAM MAKER");
-		 System.out.println("\nProvide a word, name, or phrase and out comes their anagrams.");
-		 System.out.println("You can choose the number of words in the anagram.");
-		 System.out.println("You can choose the number of anagrams shown.");
-		 System.out.println("\nLet's get started!");
-	 }
-	 
-	 /**
-	  *	Prompt the user for a phrase of characters, then create anagrams from those
-	  *	characters.
-	  */
-	 public void runAnagramMaker() {
-		 String input = Prompt.getString("Word(s), name or phrase (q to quit)");
-		 numWords = Prompt.getInt("Number of words in anagram");
-		 maxPhrases = Prompt.getInt("Maximum number of anagrams to print");
- 
-		 String phrase = "";
-		 for (int i = 0; i<input.length(); i++) { 
-			 char c = input.charAt(i);
-			 if ('a' <= c && c <= 'z' || 'A' <= c && c <= 'Z') {
-				 phrase += c;
-			 }
-		 }
- 
-		 generateAnagrams(phrase);
-	 }
- 
-	 public void generateAnagrams(String phrase) {
-		if (anagrams.size() > numWords) return;
-		if (phrase.length() >= 1) {
-			 ArrayList<String> allWords = wu.allWords(phrase);
-			 
-			 for (String s : allWords) {
-				 anagrams.add(s);
-				 String new_phrase = "";
-				 int i = 0, j = 0;
- 
-				 char[] s_arr = s.toCharArray();
-				 char[] p_arr = phrase.toCharArray();
-				 Arrays.sort(s_arr);
-				 Arrays.sort(p_arr);
- 
-				 while (i < s.length() && j < phrase.length()) {
-					 if (s_arr[i] != p_arr[j]) {
-						 new_phrase += p_arr[j];
-						 j++;
-					 }
-					 else {
-						 i++; 
-						 j++;
-					 }
-				 }
-				 for (int a = j; a<phrase.length(); a++) {
-					 new_phrase += p_arr[a];
-				 }
-				 generateAnagrams(new_phrase);
-				 anagrams.remove(s);
-			 }
-		 }
-		 else {
-			 if (numPhrases >= maxPhrases) return;
- 
-			 for (String s : anagrams) {
-				 System.out.print(s + " ");
-			 }
-			 numPhrases++;
-			 System.out.println("");
-		 }
-	 }
- }
+import java.util.ArrayList;
+
+public class AnagramMaker {
+    private final String FILE_NAME = "randomWords.txt"; // file containing all words
+
+    private WordUtilities wu; // the word utilities for building the word
+                              // database, sorting the database,
+                              // and finding all words that match
+                              // a string of characters
+
+    // variables for constraining the print output of AnagramMaker
+    private int numWords;      // the number of words in a phrase to print
+    private int maxPhrases;    // the maximum number of phrases to print
+    private int numPhrases;    // the number of phrases that have been printed
+
+    /* Initialize the database inside WordUtilities
+     * The database of words does NOT have to be sorted for AnagramMaker to work,
+     * but the output will appear in order if you DO sort.
+     */
+    public AnagramMaker() {
+        wu = new WordUtilities();
+        wu.readWordsFromFile(FILE_NAME);
+        wu.sortWords();
+
+        numPhrases = 0;
+    }
+
+    public static void main(String[] args) {
+        AnagramMaker am = new AnagramMaker();
+        am.run();
+    }
+
+    /** The top routine that prints the introduction and runs the anagram-maker.
+     */
+    public void run() {
+        printIntroduction();
+        runAnagramMaker();
+        System.out.println("\nThanks for using AnagramMaker!\n");
+    }
+
+    /**
+     * Print the introduction to AnagramMaker
+     */
+    public void printIntroduction() {
+        System.out.println("\nWelcome to ANAGRAM MAKER");
+        System.out.println("\nProvide a word, name, or phrase and out comes their anagrams.");
+        System.out.println("You can choose the number of words in the anagram.");
+        System.out.println("You can choose the number of anagrams shown.");
+        System.out.println("\nLet's get started!");
+    }
+
+    /**
+     * Prompt the user for a phrase of characters, then create anagrams from those
+     * characters.
+     */
+    public void runAnagramMaker() {
+		String input = "";
+		boolean notQ = true;
+		while (notQ) {
+			input = Prompt.getString("Word(s), name or phrase (q to quit)");
+			if (input.equals("q")) notQ = false;
+			if (notQ) {
+				numWords = Prompt.getInt("Number of words in anagram");
+				maxPhrases = Prompt.getInt("Maximum number of anagrams to print");
+
+				String phrase = "";
+				for (int i = 0; i < input.length(); i++) {
+					char c = input.charAt(i);
+					if ('a' <= c && c <= 'z' || 'A' <= c && c <= 'Z') {
+						phrase += c;
+					}
+				}
+
+				ArrayList<String> anagrams = new ArrayList<String>();
+				numPhrases = 0;
+				generateAnagrams(phrase, anagrams);
+				System.out.println("Stopped at " + numPhrases + " anagrams.");
+			}
+		}
+    }
+
+    public void generateAnagrams(String phrase, ArrayList<String> anagrams) {
+        if (!phrase.equals("")) {
+            if (anagrams.size() == numWords) {
+				for (String s : anagrams) {
+					System.out.print(s + " ");
+				}
+				System.out.println("");
+                numPhrases++;
+            }
+            return;
+        }
+        else if (anagrams.size() == numWords) {
+            return;
+        }
+		else if (numPhrases >= maxPhrases) {
+			return;
+		}
+		else {
+			ArrayList<String> allWords = wu.allWords(phrase);
+			
+			for (String s : allWords) {
+				anagrams.add(s);
+
+				String newPhrase = phrase;
+				for (int i = 0; i<s.length(); i++) {
+					char c = s.charAt(i);
+					int idx = newPhrase.indexOf(c);
+
+					if (idx >= 0) newPhrase = newPhrase.substring(0, idx) + newPhrase.substring(idx + 1);
+				}
+
+				generateAnagrams(newPhrase, anagrams);
+
+				anagrams.remove(anagrams.size() - 1);
+			}
+		}
+    }
+}
