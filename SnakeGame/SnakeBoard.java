7ecfa3be826b043db7b2bee9da0c855b0f52c315
welainom <154926099+welainom@users.noreply.github.com>
2025-05-15 22:06:39 -0700

    Update SnakeBoard.java

--- SnakeGame/SnakeBoard.java
+++ SnakeGame/SnakeBoard.java
@@ -2,30 +2,30 @@
  * 	The board for the Snake Game. Functionalities such as creating, printing
  *  adding a snake, and checking locations
  * 
- *  @author		William Liu
- * 	@since 		May 13, 2025
+ *  @author	William Liu
+ *  @since 	May 13, 2025
  */
 public class SnakeBoard {
 	/* fields */
 	private char[][] board;			// The 2D array to hold the board
 	
 	/* Constructor */
-	public SnakeBoard(int height, int width) {
-		board = new char[height + 2][width + 2];
+	public SnakeBoard(int h, int w) {
+		board = new char[h + 2][w + 2];
 		createBoard();
 	}
 	
 	/**
 	 *	Print the board to the screen.
 	 */
-	public void printBoard(Snake snake, Coordinate target) {
+	public void printBoard(Snake snake, Coordinate tgt) {
 
 		// Create board, place snake, place target
 		createBoard(); 
-        placeSnake(snake);  
-        board[target.getX()][target.getY()] = '+'; 
-        
-        // Print each cell of the board
+	        placeSnake(snake);  
+	        board[tgt.getX()][tgt.getY()] = '+'; 
+	        
+	        // Print each cell of the board
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[0].length; j++) {
 				System.out.print(board[i][j] + " "); 
@@ -72,9 +72,9 @@ public class SnakeBoard {
 	/**
 	 * This method checks if the coordinate passed in is a valid location 
 	 */
-	public boolean isValidLoc(Coordinate coord) {
-		return (coord.getY() > 0 && coord.getY() <= getWidth() &&
-				coord.getX() > 0 && coord.getX() <= getHeight());
+	public boolean isValidLoc(Coordinate c) {
+		return (c.getY() > 0 && c.getY() <= getWidth() &&
+			c.getX() > 0 && c.getX() <= getHeight());
 	}
 	
 	/**
@@ -107,9 +107,9 @@ public class SnakeBoard {
 	}
 	
 	// returns char at the coord
-	public char getChar(Coordinate coord)
+	public char getChar(Coordinate c)
 	{
-		return board[coord.getX()][coord.getY()];
+		return board[c.getX()][c.getY()];
 	}
 	
 	/********************************************************/
