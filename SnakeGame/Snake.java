78e4a586dcd207a6dabb2015f279a5dd8a5e1121
welainom <154926099+welainom@users.noreply.github.com>
2025-05-15 22:05:02 -0700

    Update Snake.java

--- SnakeGame/Snake.java
+++ SnakeGame/Snake.java
@@ -2,16 +2,16 @@
  *    A SinglyLinkedList of Coordinate objects representing
  *    a snake on a two-dimensional grid.
  *
- *	@author 	William Liu  
+ *  @author 	William Liu  
  *  @since    	May 13, 2025
  */
 
 public class Snake extends SinglyLinkedList<Coordinate> {   
 	
 	// Default constructor
-	public Snake(Coordinate loc)  {
+	public Snake(Coordinate coord)  {
 		for (int i = 0; i <= 4; i++) {
-			Coordinate c = new Coordinate(loc.getX() + i, loc.getY());
+			Coordinate c = new Coordinate(coord.getX() + i, coord.getY());
 			this.add(c);
 		}
 	}
